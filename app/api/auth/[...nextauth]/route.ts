079c569b397ea434a80006a5fab65e7d27ff35e6
garrison2025 <zhongsgeng@gmail.com>
2025-04-05 06:29:59 +0200

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,5 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
-import { Session } from 'next-auth'
-
-declare module 'next-auth' {
-  interface Session {
-    user: {
-      id?: string
-      name?: string | null
-      email?: string | null
-      image?: string | null
-    }
-  }
-}
 
 export const authOptions = {
   providers: [
